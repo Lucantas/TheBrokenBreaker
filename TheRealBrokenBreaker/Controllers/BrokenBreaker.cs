34efb07898499c807b4bf84d80e0c41d6dec6cb5
Lucas <lucas.lucantas38@gmail.com>
2018-10-21 18:33:29 -0300

    Missed changes from the last implementation

--- TheRealBrokenBreaker/Controllers/BrokenBreaker.cs
+++ TheRealBrokenBreaker/Controllers/BrokenBreaker.cs
@@ -30,9 +30,9 @@ namespace TheRealBrokenBreaker
         public static void WriteBrokenLinks(string uri, string[] badLinks)
         {
             // search for the pre-determined file to write the broken links 
-            // note the append:true option set on the StreamWriter constructor, it keeps the old links on the file
+            // note the 'append:true' option set on the StreamWriter constructor, it keeps the old links on the file
             // while inserting new lines
-            using (StreamWriter writer = new StreamWriter($@"{AppConfiguration.BrokenLinksFile}", append: true))
+            using (StreamWriter writer = new StreamWriter(AppConfiguration.BrokenLinksFile, append: true))
             {
                 // The header of that input, it is the current date followed by the uri chosen by the user
                 writer.WriteLine($"{DateTime.Now} | Bad links found on {uri}");

6adf000e9c608d346525e590b8a064ae09fc55e9
Lucas <lucas.lucantas38@gmail.com>
2018-10-20 21:52:10 -0300

    Created AppConfiguration Model

---
+++ TheRealBrokenBreaker/Models/AppConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TheRealBrokenBreaker.Models
+{
+    class AppConfiguration
+    {        
+        public static string LinkFile
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("TBB_LinkFile");
+            }
+            private set { }
+        }
+        public static string BrokenLinksFile
+        {
+            get
+            {
+                BrokenLinksFile = Environment.GetEnvironmentVariable("TBB_BrokenLinksFile");
+                return BrokenLinksFile;
+            }
+            private set { }
+        }
+        public static string LogFile
+        {
+            get
+            {
+                LogFile = Environment.GetEnvironmentVariable("TBB_LogFile");
+                return LogFile;
+            }
+            private set  { }
+        }
+        public static string JSONConfigs
+        {
+            get
+            {
+                JSONConfigs = Environment.GetEnvironmentVariable("TBB_JSONConfigs");
+                return JSONConfigs;
+            }
+            private set { }
+        }
+    }
+}
